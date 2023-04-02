784c6a3d5de5bcffb87cb84012c3655717cc0da1
LeandroTorresSicilia <jtorressicilia@gmail.com>
2023-04-01 20:04:20 -0700

    fix: add react rainbow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        // add alias for importing without specifying index.js
+        '@button': path.resolve('node_modules/react-rainbow-components/components/Button')
+      },
+      fallback: {
+        ...config.resolve.fallback,
+        fs: false // this is necessary to prevent a specific error that can occur when using path.resolve
+      }
+    };
+    return config;
+  }
 }
 
 module.exports = nextConfig
